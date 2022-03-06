16d9b4bfbc6532a38d2c2854b61d57750a497425
Aaron Cohen <acohen@mountainpasstech.com>
2022-03-06 15:00:05 +0200

    fix test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 struct Endpoint {
-    name: &'static str,
-    uri: &'static str,
-    methods: Vec<&'static str>,
+    name: String,
+    uri: String,
+    methods: Vec<String>,
 }
 impl Endpoint {
-    fn implements_method(&self, method: &str) -> bool {
+    fn implements_method(&self, method: String) -> bool {
         self.methods.contains(&method)
     }
 }
@@ -12,20 +12,20 @@ impl Endpoint {
 #[test]
 fn implements_method() {
     let e = Endpoint {
-        name: "foo",
-        uri: "/foo",
-        methods: vec!["GET"],
+        name: "foo".to_string(),
+        uri: "/foo".to_string(),
+        methods: vec!["GET".to_string()],
     };
-    assert_eq!(e.implements_method("GET"), true);
+    assert_eq!(e.implements_method("GET".to_string()), true);
 }
 #[test]
 fn doesnt_implement_method() {
     let e = Endpoint {
-        name: "foo",
-        uri: "/foo",
-        methods: vec!["GET"],
+        name: "foo".to_string(),
+        uri: "/foo".to_string(),
+        methods: vec!["GET".to_string()],
     };
-    assert_eq!(e.implements_method("POST"), false);
+    assert_eq!(e.implements_method("POST".to_string()), false);
 }
 
 fn main() {
