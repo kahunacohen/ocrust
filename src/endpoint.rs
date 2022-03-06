d9eda72db14e0d67a23081e96544080cab1a6fa4
Aaron Cohen <acohen@mountainpasstech.com>
2022-03-06 17:45:18 +0200

    add endpoint

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,9 +1,25 @@
+fn normalize_uri(uri: String) -> String {
+    if !uri.ends_with("/") {
+        return format!("{}/", uri);
+    } else {
+        return uri;
+    }
+}
+
 pub struct Endpoint {
     name: String,
     uri: String,
     methods: Vec<String>,
 }
 impl Endpoint {
+    fn new(name: String, uri: String, methods: Vec<String>) -> Endpoint {
+        Endpoint {
+            name,
+            uri: normalize_uri(uri),
+            methods,
+        }
+    }
+
     fn implements_method(&self, method: String) -> bool {
         self.methods.contains(&method)
     }
@@ -14,6 +30,16 @@ mod test {
     use super::*;
 
     #[test]
+    fn new_adds_trailing_slash_to_uri() {
+        let e = Endpoint::new(
+            "foo".to_string(),
+            "/foo".to_string(),
+            vec!["GET".to_string()],
+        );
+        assert_eq!(e.uri, "/foo/");
+    }
+
+    #[test]
     fn implements_method() {
         let e = Endpoint {
             name: "foo".to_string(),
@@ -22,7 +48,7 @@ mod test {
         };
         assert_eq!(e.implements_method("GET".to_string()), true);
     }
-    
+
     #[test]
     fn doesnt_implement_method() {
         let e = Endpoint {
