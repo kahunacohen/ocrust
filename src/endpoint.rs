9a13f0f58985b726fbe66b12c3240783ae02a6d0
Aaron Cohen <acohen@mountainpasstech.com>
2022-05-10 17:22:07 +0300

    add docs

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -7,15 +7,25 @@ pub enum Method {
     GET,
 }
 
-// A "base" payload
+/// A "generic" payload we can use as a type parameter
+/// to Endpoint when instantiating. Otherwise the compiler is not
+/// happy. For example, when we instantiate an endpoint and assign it
+/// to a variable, the type checker isn't happy unless we provide explicit
+/// annotation on the variable:
+/// ```
+/// let e: Endpoint<Payload> = Endpoint::new(
+///     String::from("/foo"),
+///     HashMap::from([(Method::GET, None)]),
+/// );
+/// ```
 pub struct Payload {
     data: Option<String>
 }
-// Represents an API endpoint. The
-// generic parameter `P` stands for some kind
-// of payload. So the methods field is a vector
-// of hashmaps that map HTTP methods, "GET" etc. to
-// a payload.
+/// Represents an API endpoint. The
+/// generic parameter `P` stands for some kind
+/// of payload. So the methods field is a vector
+/// of hashmaps that map HTTP methods, "GET" etc. to
+/// a payload.
 pub struct Endpoint<P> {
     pub uri: String,
     pub methods: HashMap<Method, Option<P>>,
