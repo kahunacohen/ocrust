e9556f1ca10a8630926f18ae0dcc90166dcbfb49
Aaron Cohen <acohen@mountainpasstech.com>
2022-05-15 18:41:11 +0300

    get request working

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use crate::endpoint::Endpoint;
 use crate::functions;
+use serde::Deserialize;
 use std::collections::HashMap;
 use ureq;
 
@@ -16,7 +17,8 @@ impl<P> Server<'_, P> {
         }
     }
     fn request(&self, method: &str, uri: String) -> Result<ureq::Response, ureq::Error> {
-        ureq::request(&method.to_string(), "https://www.google.com").call()
+        print!("{:?}\n", uri);
+        ureq::request(&method, "https://www.google.com").call()
     }
     // pub fn get(&self, uri: String) -> String {
     //     self.request(Method::GET, uri)
@@ -25,6 +27,8 @@ impl<P> Server<'_, P> {
 
 #[cfg(test)]
 mod test {
+    use ureq::serde_json;
+
     use super::*;
 
     #[test]
@@ -38,5 +42,22 @@ mod test {
             )],
         );
         assert_eq!(server.base_url, "https://papi.dev.ocdvlp.com/opportunities");
+    }
+    #[test]
+
+    fn get() {
+        let server = Server::new(
+            "https://papi.dev.ocdvlp.com/opportunities/".to_string(),
+            vec![Endpoint::new(
+                "/opportunities",
+                "A test endpoint",
+                HashMap::from([("GET", Some("x".to_string()))]),
+            )],
+        );
+        let resp = server
+            .request("GET", String::from("/opportunities"))
+            .unwrap();
+        assert_eq!(resp.status(), 200);
+        println!("{:?}", resp.get_url());
     }
 }
